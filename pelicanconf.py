0a4fc117d287c338d8a1a467f99372b6fd0a0ae3
johngolden <johngolden@gmail.com>
2018-07-29 09:44:42 +0200

    initial push

---
+++ pelicanconf.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'John Golden'
+SITENAME = 'John Golden'
+SITESUBTITLE = ''
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'America/Denver'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Set the article URL
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+#MARKUP = ('md', 'ipynb')
+#PLUGINS = ['ipynb.markup']
+
+MARKUP = ['md']
+PLUGIN_PATHS = ['./plugins', './plugins/pelican-plugins']
+PLUGINS = [
+    'summary',       # auto-summarizing articles
+    'feed_summary',  # use summaries for RSS, not full articles
+    'liquid_tags.img',  # embedding images
+    'liquid_tags.include_code',  # including code blocks
+    'liquid_tags.literal'
+]
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# for liquid tags
+CODE_DIR = 'downloads/code'
+NOTEBOOK_DIR = 'downloads/notebooks'
+
+# THEME SETTINGS
+THEME = './theme/'
+
+ABOUT_PAGE = ''
+TWITTER_USERNAME = ''
+GITHUB_USERNAME = 'johngolden'
+STACKOVERFLOW_ADDRESS = ''
+AUTHOR_WEBSITE = 'http://johngolden.github.io/'
+AUTHOR_CV = ''
+SHOW_ARCHIVES = True
+SHOW_FEED = False  # Need to address large feeds
+
+ENABLE_MATHJAX = False
+
+STATIC_PATHS = ['images', 'favicon.ico']
+
+# Footer info
+
+LICENSE_URL = ''
+LICENSE = "MIT"
